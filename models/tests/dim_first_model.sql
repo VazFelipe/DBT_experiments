dbabb0dee77ac199ee71ef451f7b352c9726744c
VazFelipe <57250903+VazFelipe@users.noreply.github.com>
2023-01-23 07:06:22 -0300

    Development (#6)
    * Adding modifications on README.md and a new query for enviroment testing.
    
    * Adding dim_first_model
    
    * Adding configs on the dim_first_model model
    
    * dding changes on README.md
    
    * Adding stg_payments, fct_orders and update dim_customers
    
    Co-authored-by: felipe.vaz@localiza.com <felipe.vaz@localiza.com>

--- models/tests/dim_first_model.sql
+++ models/tests/dim_first_model.sql
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 {{
     config(
         materialized='table'
     )
 }}
 
-=======
->>>>>>> dec210c4ee6ffb958f991e5e187440e69233564a
 {% for i in range(10) %}
 
 select {{ i }} as number
